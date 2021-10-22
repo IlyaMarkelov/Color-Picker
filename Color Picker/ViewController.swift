7dab0ad841429782b4442e4ec7970bbcd62fea20
IlyaMarkelov <marckellow@icloud.com>
2021-10-22 15:45:05 +0300

    Создание приложения Color Picker

--- Color Picker/ViewController.swift
+++ Color Picker/ViewController.swift
@@ -14,44 +14,74 @@ class ViewController: UIViewController {
     @IBOutlet var greenRageLabel: UILabel!
     @IBOutlet var blueRageLabel: UILabel!
     
+    
     @IBOutlet var redSlider: UISlider!
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var cleanButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         colorView.layer.cornerRadius = 20
+        colorView.backgroundColor = .black
         
         redSlider.value = 0
         redSlider.minimumValue = 0
         redSlider.maximumValue = 1
+        redRangeLabel.text = String(format: "%.2f", redSlider.value)
         
         greenSlider.value = 0
         greenSlider.minimumValue = 0
         greenSlider.maximumValue = 1
+        greenRageLabel.text = String(format: "%.2f", greenSlider.value)
 
         blueSlider.value = 0
         blueSlider.minimumValue = 0
         blueSlider.maximumValue = 1
+        blueRageLabel.text = String(format: "%.2f", blueSlider.value)
         
-//        redRangeLabel.text = String(redSlider.value)
-
+        cleanButton.layer.cornerRadius = 15
+        
     }
 
-    @IBAction func redSliderAction() {
+    @IBAction func redSliderAction(_ sender: UISlider) {
+        setColor()
         redRangeLabel.text = String(format: "%.2f", redSlider.value)
-        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(redSlider.value),  blue: CGFloat(redSlider.value), alpha: 1.0)
     }
     
+    
     @IBAction func greenSliderAction() {
+        setColor()
         greenRageLabel.text = String(format: "%.2f", greenSlider.value)
-        colorView.backgroundColor = UIColor(red: CGFloat(greenSlider.value), green: CGFloat(greenSlider.value),  blue: CGFloat(greenSlider.value), alpha: 1.0)
     }
     
     @IBAction func blueSliderAction() {
+        setColor()
         blueRageLabel.text = String(format: "%.2f", blueSlider.value)
     }
     
+    @IBAction func cleanViewAndSlides() {
+        colorView.backgroundColor = .black
+        
+        redSlider.value = 0
+        redRangeLabel.text = String(format: "%.2f", redSlider.value)
+
+        greenSlider.value = 0
+        greenRageLabel.text = String(format: "%.2f", greenSlider.value)
+
+        blueSlider.value = 0
+        blueRageLabel.text = String(format: "%.2f", blueSlider.value)
+
+    }
+    
+    
+    private func setColor() {
+        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value),
+                                            green: CGFloat(greenSlider.value),
+                                            blue: CGFloat(blueSlider.value),
+                                            alpha: 1)
+    }
 }
 
